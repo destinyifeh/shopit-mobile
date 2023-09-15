42bc0f246a2304f35f2fb972127368eac3fcec19
Destiny Ifehor <Destiny.Ifehor@interswitchgroup.com>
2023-09-16 00:44:51 +0100

    app configuration in app.json file

--- app/index.js
+++ app/index.js
@@ -1,5 +1,18 @@
 import { Redirect } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 import React from "react";
+
 export default function App() {
+  // React.useEffect(() => {
+  //   setTimeout(async () => {
+  //     await SplashScreen.hideAsync();
+  //   }, 3000);
+  // }, []);
+  SplashScreen.preventAutoHideAsync()
+    .then((result) =>
+      console.log(`SplashScreen.preventAutoHideAsync() succeeded: ${result}`)
+    )
+    .catch(console.warn);
+
   return <Redirect href="/home" />;
 }
