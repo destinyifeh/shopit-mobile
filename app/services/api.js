c5feed01ff34c818fccdf4d3370e52715666101b
Destiny Ifehor <Destiny.Ifehor@interswitchgroup.com>
2023-09-18 00:47:09 +0100

    updated

--- app/services/api.js
+++ app/services/api.js
@@ -4,5 +4,5 @@ export const requester = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://192.168.19.33:5000/api"
-      : "https://shopit-etn6.onrender.com",
+      : "https://shopit-etn6.onrender.com/api",
 });
